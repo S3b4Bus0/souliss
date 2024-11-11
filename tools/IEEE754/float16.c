2b9c87ce27db0f6d817b8632e080e4708505d01d
lucacalcaterra <calcaterra.luca@gmail.com>
2024-11-11 18:43:58 +0100

    Update float16.c
    Fixed float16 lib for ESP32 (original not works)

--- tools/IEEE754/float16.c
+++ tools/IEEE754/float16.c
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	
 	Modified by Dario Di Maio
+	Updated by Luca Calcaterra for ESP32
 	
 ***************************************************************************/
 
@@ -83,130 +84,136 @@
 /*!
 	Convert a single precision floating point into an half precision one
 */	
-/**************************************************************************/ 
+/**************************************************************************/
 void float16(uint16_t *output, float *input)
 {
-    uint16_t *hp = (uint16_t*) output; 	
-    uint16_t	hs, he, hm;
-    uint32_t	x, xs, xe, xm;
-    int hes;
-    
-	x = *(uint32_t*) input;
+	uint16_t *hp = (uint16_t *)output;
+	uint16_t hs, he, hm;
+	uint32_t x, xs, xe, xm;
+	int hes;
 
-	if( (x & 0x7FFFFFFF) == 0 ) 										// Signed zero   
-		*hp = 0x8000; 													// Return the signed zero
-	else 
-	{ 
-		// Not zero
-		xs = x & 0x80000000;  											// Pick off sign bit
-		xe = x & 0x7F800000;  											// Pick off exponent bits
-		xm = x & 0x007FFFFF;  											// Pick off mantissa bits
-            
-		if( xe == 0 ) 													// Denormal will underflow, return a signed zero
-			*hp = (uint16_t) (xs >> 16);
-		else if( xe == 0x7F800000) 
-		{  
-			// Inf or NaN (all the exponent bits are set)
-			if( xm == 0 ) 
-				*hp = (uint16_t) ((xs >> 16) | 0x7C00); 				// Mantissa is zero, so convert as signed Inf
-			else 
-				*hp = (uint16_t) 0xFE00; 								// NaN, only 1st mantissa bit set
-		}	
-		else 
-		{ 
-			// Normalized number
-			hs = (uint16_t) (xs >> 16); 							// Sign bit
-			hes = ((int)(xe >> 23)) - 127 + 15; 					// Exponent unbias the single, then bias the halfp
-			if( hes >= 0x1F ) 										// Overflow
-				*hp = (uint16_t) ((xs >> 16) | 0x7C00); 			// Signed Inf
-			else if( hes <= 0 ) 								  	// Underflow
+	// Utilizzo di memcpy al posto di cast diretti
+	memcpy(&x, input, sizeof(x));
+
+	if ((x & 0x7FFFFFFF) == 0)
+	{
+		*hp = 0x8000;
+	}
+	else
+	{
+		xs = x & 0x80000000;
+		xe = x & 0x7F800000;
+		xm = x & 0x007FFFFF;
+
+		if (xe == 0)
+		{
+			*hp = (uint16_t)(xs >> 16);
+		}
+		else if (xe == 0x7F800000)
+		{
+			if (xm == 0)
+				*hp = (uint16_t)((xs >> 16) | 0x7C00);
+			else
+				*hp = (uint16_t)0xFE00;
+		}
+		else
+		{
+			hs = (uint16_t)(xs >> 16);
+			hes = ((int)(xe >> 23)) - 127 + 15;
+			if (hes >= 0x1F)
+				*hp = (uint16_t)((xs >> 16) | 0x7C00);
+			else if (hes <= 0)
 			{
-				if( (14 - hes) > 24 )								// Mantissa shifted all the way off & no rounding possibility
-					hm = (uint16_t) 0;  							// Set mantissa to zero
+				if ((14 - hes) > 24)
+					hm = (uint16_t)0;
 				else
 				{
-					xm |= 0x00800000;  								// Add the hidden leading bit
-					hm = (uint16_t) (xm >> (14 - hes)); 			// Mantissa
-					if( (xm >> (13 - hes)) & 0x00000001 ) 			// Check for rounding
-						hm += (uint16_t) 1; 						// Round, might overflow into exp bit, but this is OK
+					xm |= 0x00800000;
+					hm = (uint16_t)(xm >> (14 - hes));
+					if ((xm >> (13 - hes)) & 0x00000001)
+						hm += (uint16_t)1;
 				}
-				
-				*hp = (hs | hm); 									// Combine sign bit and mantissa bits, biased exponent is zero
-			} 
-			else 
+
+				*hp = (hs | hm);
+			}
+			else
 			{
-				he = (uint16_t) (hes << 10); 						// Exponent
-				hm = (uint16_t) (xm >> 13); 						// Mantissa
-				if( xm & 0x00001000 ) 								// Check for rounding
-					*hp = (hs | he | hm) + (uint16_t) 1; 			// Round, might overflow to inf, this is OK
+				he = (uint16_t)(hes << 10);
+				hm = (uint16_t)(xm >> 13);
+				if (xm & 0x00001000)
+					*hp = (hs | he | hm) + (uint16_t)1;
 				else
-					*hp = (hs | he | hm);  							// No rounding
+					*hp = (hs | he | hm);
 			}
 		}
 	}
 }
 
-
 /**************************************************************************/
 /*!
 	Convert an half precision floating point into an single precision one
 */	
-/**************************************************************************/ 
+/**************************************************************************/
 void float32(uint16_t *input, float *output)
 {
-    uint16_t *hp = (uint16_t *) input;
-    uint32_t *xp = (uint32_t *) output; 
-    uint16_t h, hs, he, hm;
-    uint32_t xs, xe, xm;
-    int32_t xes;
-    int e;
-    
-	h = *hp;
-	if( (h & 0x7FFF) == 0 )   											// Signed zero
-		*xp = ((uint32_t) h) << 16;  									// Return the signed zero
-	else 																// Not zero
-	{ 
-		hs = h & 0x8000;  												// Pick off sign bit
-		he = h & 0x7C00;  												// Pick off exponent bits
-		hm = h & 0x03FF;  												// Pick off mantissa bits
-		if( he == 0 ) 													// Denormal will convert to normalized
-		{  
-			e = -1; 													// The following loop figures out how much extra to adjust the exponent
-			do 
+	uint16_t *hp = (uint16_t *)input;
+	uint32_t x, xs, xe, xm;
+	uint16_t h, hs, he, hm;
+	int32_t xes;
+	int e;
+
+	memcpy(&h, hp, sizeof(h));
+
+	if ((h & 0x7FFF) == 0)
+	{
+		x = ((uint32_t)h) << 16;
+	}
+	else
+	{
+		hs = h & 0x8000;
+		he = h & 0x7C00;
+		hm = h & 0x03FF;
+
+		if (he == 0)
+		{
+			e = -1;
+			do
 			{
 				e++;
 				hm <<= 1;
-			} 
-			while( (hm & 0x0400) == 0 ); 								// Shift until leading bit overflows into exponent bit
-			
-			xs = ((uint32_t) hs) << 16; 								// Sign bit
-			xes = ((uint32_t) (he >> 10)) - 15 + 127 - e; 				// Exponent unbias the halfp, then bias the single
-			xe = (uint32_t) (xes << 23); 								// Exponent
-			xm = ((uint32_t) (hm & 0x03FF)) << 13; 						// Mantissa
-			*xp++ = (xs | xe | xm); 									// Combine sign bit, exponent bits, and mantissa bits
+			} while ((hm & 0x0400) == 0);
+
+			xs = ((uint32_t)hs) << 16;
+			xes = ((int32_t)(he >> 10)) - 15 + 127 - e;
+			xe = (uint32_t)(xes << 23);
+			xm = ((uint32_t)(hm & 0x03FF)) << 13;
+			x = (xs | xe | xm);
 		}
-		else if( he == 0x7C00 ) 										// Inf or NaN (all the exponent bits are set)
-		{  
-			if( hm == 0 )  												
-				*xp++ = (((uint32_t) hs) << 16) | ((uint32_t) 0x7F800000); //Mantissa is zero convert as signed Inf
-			else 
-				*xp++ = (uint32_t) 0xFFC00000; 							// NaN, only 1st mantissa bit set
-		} 
-		else 															// Normalized number
-		{ 
-			xs = ((uint32_t) hs) << 16; 								// Sign bit
-			xes = ((int32_t) (he >> 10)) - 15 + 127; 					// Exponent unbias the halfp, then bias the single
-			xe = (uint32_t) (xes << 23); 								// Exponent
-			xm = ((uint32_t) hm) << 13; 								// Mantissa
-			*xp++ = (xs | xe | xm); 									// Combine sign bit, exponent bits, and mantissa bits
+		else if (he == 0x7C00)
+		{
+			if (hm == 0)
+				x = (((uint32_t)hs) << 16) | ((uint32_t)0x7F800000);
+			else
+				x = (uint32_t)0xFFC00000;
+		}
+		else
+		{
+			xs = ((uint32_t)hs) << 16;
+			xes = ((int32_t)(he >> 10)) - 15 + 127;
+			xe = (uint32_t)(xes << 23);
+			xm = ((uint32_t)hm) << 13;
+			x = (xs | xe | xm);
 		}
 	}
+
+	// Copia il risultato nella variabile di output
+	memcpy(output, &x, sizeof(x));
 }
 
 float returnfloat32(uint16_t *input)
 {
 	float output;
-	float32(input,  &output);
+	float32(input, &output);
 	return output;
 }
 
